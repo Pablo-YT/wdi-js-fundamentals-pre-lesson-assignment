4d764ccff7b8dd1697263a3ca19c9475de922776
Josh_Date <jjdate@live.com>
2019-03-22 16:04:38 -0400

    done

---
+++ pacman-console-log.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+console.log("Pacman eats GHOSTS")
+//I am a comment
+/* total news
+
+this is a multi line comment*/
+
+const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+console.log(`There are ${ghosts.length} ghosts.`); //
+console.log("There NAMES ARE:")
+console.log(ghosts[0])
+console.log(ghosts[1])
+console.log(ghosts[2])
+console.log(ghosts[3])
